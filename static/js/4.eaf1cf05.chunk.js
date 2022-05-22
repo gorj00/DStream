2b5b54c29390c2ec9503b796e462cbaa65616573
Jan Gorol <os.jangorol@gmail.com>
2022-05-22 21:19:59 +0200

    Updates

---
+++ static/js/4.eaf1cf05.chunk.js
@@ -0,0 +1,19 @@
+/*!
+
+=========================================================
+* BLK Design System React - v1.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/blk-design-system-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/blk-design-system-react/blob/main/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+(this.webpackJsonpdstream=this.webpackJsonpdstream||[]).push([[4],{306:function(e,s,c){"use strict";c.r(s);c(2);var n=c(264),a=c(265),t=c(107),i=c(0);s.default=function(){return Object(i.jsxs)("div",{className:"content-center brand",children:[Object(i.jsx)("h1",{className:"h1-seo",children:"DStream"}),Object(i.jsx)("h3",{className:"d-none d-sm-block",children:"Master thesis Dapp prototype of decentralized video straming and content production service"}),Object(i.jsxs)(n.a,{className:"justify-content-md-center",children:[Object(i.jsx)(a.a,{md:"5",children:Object(i.jsxs)(t.a,{className:"nav-link d-none d-lg-block",color:"primary",size:"lg",children:[Object(i.jsx)("i",{className:"tim-icons icon-spaceship"})," Sign Up"]})}),Object(i.jsx)(a.a,{md:"5",children:Object(i.jsxs)(t.a,{className:"nav-link d-none d-lg-block",color:"default",size:"lg",children:[Object(i.jsx)("i",{className:"tim-icons icon-cloud-download-93"})," Log In"]})})]})]})}}}]);
+//# sourceMappingURL=4.eaf1cf05.chunk.js.map
