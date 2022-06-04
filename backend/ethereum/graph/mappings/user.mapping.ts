f6f03a2455bf5b6df8675d5300e2fd840ab9d6de
Jan Gorol <os.jangorol@gmail.com>
2022-06-04 06:21:40 +0200

    user mapping

--- backend/ethereum/graph/mappings/user.mapping.ts
+++ backend/ethereum/graph/mappings/user.mapping.ts
@@ -2,17 +2,21 @@ import { UserRegistered, UserLoginLog } from '../generated/User/User'
 import { User } from '../generated/schema'
 
 export function handleRegistration(event: UserRegistered): void {
-  const user = new Gravatar(event.params.id)
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+  const user = new User(event.params.addr.toHex())
+  user.addr = event.params.addr.toHex()
+  user.isLoggedIn = false
+  user.date = event.block.timestamp
+  user.save()
 }
 
-export function handleNewGravatar(event: NewGravatar): void {
-  let gravatar = new Gravatar(event.params.id)
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+export function handleUserLoginLog(event: UserLoginLog): void {
+  const id = event.params.addr.toHex()
+  let user = User.load(id)
+  if (!user) {
+    user = new User(id)
+  }
+  user.addr = event.params.addr.toHex()
+  user.isLoggedIn = event.params.isLoggedIn
+  user.date = event.params.date
+  user.save()
 }
